faa50e07c90e3b31b36bf17399ea1707fa7815e2
Shridhan <shridhan.suman2023@vitstudent.ac.in>
2025-10-22 12:13:06 +0530

    education form

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -18,6 +18,7 @@ import TemplateSelector from "../components/TemplateSelector";
 import ColorPicker from "../components/ColorPicker";
 import ProfessionalSummaryForm from "../components/ProfessionalSummaryForm";
 import Experience from "../components/Experience";
+import EducationForm from "../components/EducationForm";
 
 const ResumeBuilder = () => {
   const { resumeId } = useParams();
@@ -178,7 +179,18 @@ const ResumeBuilder = () => {
                         ...prev,
                         experience: data,
                       }))
-                    } 
+                    }
+                  />
+                )}
+                {activeSection.id === "education" && (
+                  <EducationForm
+                    data={resumeData.education}
+                    onChange={(data) =>
+                      setResumeData((prev) => ({
+                        ...prev,
+                        education: data,
+                      }))
+                    }
                   />
                 )}
               </div>
